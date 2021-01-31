56cbd3aae91e45bb9fd445e61c5346752129ef9f
temizkanekin <emtemizkan@Ekin-MacBook-Pro.local>
2021-01-31 10:08:33 +0300

    lighthouse.js updated

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,13 @@
 module.exports = {
   ci: {
+    collect: {
+      url: [
+        "https://festive-lumiere-02399a.netlify.app/",
+        "https://festive-lumiere-02399a.netlify.app/salesmarket",
+        "https://festive-lumiere-02399a.netlify.app/salesmarket/20533391"
+      ],
+      numberOfRuns: 2
+    },
     upload: {
       target: 'temporary-public-storage',
     },
